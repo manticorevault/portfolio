b5f03c249b8eef80e4b3b23563f3e016dd967559
Artur Serra <artur_seilah@hotmail.com>
2023-04-13 20:57:46 -0300

    Updated with the resume and made it downloadable

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
               </p>
               <div className='flex items-center self-start mt-2'>
                 <Link 
-                  href="/arturserra_resume.pdf" target={"_blank"}
+                  href="/artur-serra-resume.pdf" target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6
                               rounded-lg text-lg font-semibold hover:bg-light 
                               hover:text-dark border-2 border-solid border-transparent
